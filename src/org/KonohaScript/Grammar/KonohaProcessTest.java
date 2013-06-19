6c19b6d20ec3d8fa8f7532503c4ff82c740c44fd
Tetsuro Matsumura <matsumura.t.lab@gmail.com>
2013-06-20 08:30:45 +0900

    fix process test

--- src/org/KonohaScript/Grammar/KonohaProcessTest.java
+++ src/org/KonohaScript/Grammar/KonohaProcessTest.java
@@ -13,7 +13,7 @@ public class KonohaProcessTest extends KTestCase {
 	}
 
 	void testEchoWithNoArgument() {
-		Process Proc1 = new Process("notepad");
+		Process Proc1 = new Process("echo");
 		Proc1.start();
 		Proc1.waitFor();
 		this.AssertEqual(Proc1.getStdout(), "\n");
@@ -21,7 +21,7 @@ public class KonohaProcessTest extends KTestCase {
 	}
 
 	void testSimpleEcho() {
-		Process Proc1 = new Process("notepad");
+		Process Proc1 = new Process("echo");
 		String Args1[] = { "1234" };
 		Proc1.setArgument(Args1);
 		Proc1.start();
