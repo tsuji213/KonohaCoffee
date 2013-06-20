182b4d4e5e1028caed0924af94cc21a9cd262fbb
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2013-06-20 12:38:03 +0900

    enable systemcall trace

--- src/org/KonohaScript/Grammar/KonohaProcessDef.java
+++ src/org/KonohaScript/Grammar/KonohaProcessDef.java
@@ -1,6 +1,7 @@
 package org.KonohaScript.Grammar;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -8,6 +9,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import org.KonohaScript.KonohaDef;
 import org.KonohaScript.KonohaNameSpace;
@@ -112,6 +114,15 @@ public class KonohaProcessDef extends KonohaDef implements KonohaConst {
 	public static int GetRetValue(KonohaProcess Process) {
 		return Process.getRet();
 	}
+	
+	// main()
+	public static void main(String[] args) {
+		KonohaProcess proc1 = new KonohaProcess("ls", true);
+		proc1.setArgument("/root");
+		proc1.start();
+		System.out.print("<<stdout>>\n" + proc1.getStdout());
+		System.err.print("<<stderr>>\n" + proc1.getStderr());
+	}
 }
 
 class KonohaProcess {
@@ -121,16 +132,62 @@ class KonohaProcess {
 	private InputStream				stdout	= null;
 	private InputStream				stderr	= null;
 
-	private String			command;
-	private ArrayList<String>	Arguments;
-
+	private ArrayList<String>	commandList;
+	private boolean enableSyscallTrace = false;
+	private final String logdirPath = "/tmp/strace-log";
+	private String logFilePath;
+	
+	
 	public KonohaProcess(String command) {
-		this.command = command;
-		this.Arguments = new ArrayList<String>();
+		this.commandList = new ArrayList<String>();
+		this.commandList.add(command);
+	}
+	
+	public KonohaProcess(String command, boolean enableSyscallTrace) {
+		this.commandList = new ArrayList<String>();
+		this.enableSyscallTrace = enableSyscallTrace;
+		
+		if (this.enableSyscallTrace) {
+			String currentLogdirPath = createLogDirectory();
+			String logNameHeader = createLogNameHeader();
+			logFilePath = new String(currentLogdirPath + "/" + logNameHeader + ".log");
+			
+			String[] straceCmd = {"strace", "-t", "-f", "-F", "-o", logFilePath};
+			for (int i = 0; i < straceCmd.length; i++) {
+				this.commandList.add(straceCmd[i]);
+			}
+		}
+		this.commandList.add(command);
+	}
+	
+	private String createLogDirectory() {
+		Calendar cal = Calendar.getInstance();
+		String subdirName = "";
+		
+		subdirName = subdirName.concat(cal.get(Calendar.YEAR) + "-");
+		subdirName = subdirName.concat((cal.get(Calendar.MONTH) + 1) + "-");
+		subdirName = subdirName.concat(cal.get(Calendar.DATE) + "");
+		
+		String subdirPath = new String(logdirPath + "/" + subdirName);
+		File subdir = new File(subdirPath);
+		subdir.mkdirs();
+		
+		return subdirPath;
+	}
+	
+	private String createLogNameHeader() {
+		Calendar cal = Calendar.getInstance();
+		String logNameHeader = "";
+		
+		logNameHeader = logNameHeader.concat(cal.get((Calendar.HOUR) + 1) + ":");
+		logNameHeader = logNameHeader.concat(cal.get(Calendar.MINUTE) + "-");
+		logNameHeader = logNameHeader.concat(cal.get(Calendar.MILLISECOND) + "");
+		
+		return logNameHeader;
 	}
 
 	public void setArgument(String Arg) {
-		this.Arguments.add(Arg);
+		this.commandList.add(Arg);
 	}
 
 	public void setArgument(String[] Args) {
@@ -139,12 +196,11 @@ class KonohaProcess {
 		}
 	}
 
-	public void start() {
-		int size = this.Arguments.size() + 1;
+	public void start() {		
+		int size = this.commandList.size();
 		String[] cmd = new String[size];
-		cmd[0] = this.command;
-		for(int i = 1; i < size; i++) {
-			cmd[i] = this.Arguments.get(i - 1);
+		for (int i = 0; i < size; i++) {
+			cmd[i] = this.commandList.get(i);
 		}
 
 		try {
@@ -229,6 +285,8 @@ class KonohaProcess {
 	public void kill() {
 		this.proc.destroy();
 	}
+	
+	
 }
 
 class StreamGetter extends Thread {
